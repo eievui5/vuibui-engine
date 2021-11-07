d74292e56f41c31b96d7ffae235ffdba2b9d9218
Eievui <14899090+GreenAndEievui@users.noreply.github.com>
2021-11-07 18:09:02 -0500

    Fixed bugs in Interp8.

--- src/algo.asm
+++ src/algo.asm
@@ -1,21 +1,37 @@
 SECTION "Interpolate 8-bit", ROM0
-; Signed interpolation towards an 8 bit value.
+; Unsigned interpolation towards an 8 bit value.
 ; @in  b: Current value.
 ; @in  c: Target value
 ; @in  d: Interpolation factor.
 ; @out b: Resulting value.
 Interp8::
+	; Don't interpolate if you already reached the target.
+	ld a, b
+	cp a, c
+	ret z
+	; Determine the direction of interpolation.
+	ld a, c
+	sub a, b
+	bit 7, a
+	jr z, :+
+	ld a, d
+	cpl
+	inc a
+	ld d, a
+:
 	; Add the interpolation factor to the current value.
 	ld a, b
 	add a, d
 	ld b, a
 	; Check if the value reached its target or overflowed.
-	sub a, c
+	cp a, c
 	ret z
 	; A now contains the different between the value and its target.
 	; If this is smaller than the interpolation factor, clamp the value and
 	; return.
 	; if (abs(target - current) < abs(factor)) return target else return current;
+	ld a, c
+	sub a, b
 	; abs(target - current)
 	bit 7, a
 	jr z, :+
@@ -24,7 +40,7 @@ Interp8::
 :
 	ld e, a ; Store the difference in c.
 	; abs(factor)
-	ld a, b
+	ld a, d
 	bit 7, a
 	jr z, :+
 	cpl
